c19faac8066fa9f44069ea66f4d8d65eae855434
Alex Reckard <alexreckard@gmail.com>
2023-06-06 04:12:20 -0500

    dear god please work (╯°□°）╯︵ ┻━┻

--- js/scroll-smooth.js
+++ js/scroll-smooth.js
@@ -11,13 +11,14 @@ $(document).ready(function() {
   
         // Using jQuery's animate() method to add smooth page scroll
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-     
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
+        if ($(hash).length) {
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+            window.location.hash = hash;
+          });
+       }
+       
       } 
     });
   });
