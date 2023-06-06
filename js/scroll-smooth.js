e06f6528d8ad968c160eeda663a76e679a053cd1
Alex Reckard <alexreckard@gmail.com>
2023-06-06 14:21:59 -0500

    changed some photos and alignment things

--- js/scroll-smooth.js
+++ js/scroll-smooth.js
@@ -1,25 +1,27 @@
 $(document).ready(function() {
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        const hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        if ($(hash).length) {
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top
-          }, 800, function(){
-            window.location.hash = hash;
-          });
-       }
-       
-      } 
-    });
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      const hash = this.hash;
+
+      // Calculate the height of your header
+      const headerHeight = $('.navbar').outerHeight();
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      if ($(hash).length) {
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top - headerHeight
+        }, 800, function(){
+          // Update the URL without adding a history entry
+          history.replaceState(null, null, hash);
+        });
+      }
+    } 
   });
-  
+});
